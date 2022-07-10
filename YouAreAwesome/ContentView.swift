29dd8f39f7f58d0d0a9ce888bd55943aa5c48c85
Derek Awa <dawa82@hotmail.com>
2022-07-10 14:41:54 -0700

    First commit

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var messageString = "Swift"
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .frame(height: 150)
+                .frame(maxWidth: UIScreen.main.bounds.width)
+                .padding()
+            
+            Image(systemName: "cloud.sun.rain.fill")
+                .symbolRenderingMode(.multicolor)
+                .resizable()
+                .padding()
+                .background(Color(red: 0.329, green: 0.863, blue: 0.918))
+                .cornerRadius(30)
+                .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 1)
+                )
+                .aspectRatio(contentMode: .fit)
+                .padding()
+                
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the button is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
             .padding()
+        }
     }
 }
 
